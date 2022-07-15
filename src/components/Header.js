b23c2d32c8c8eff0f6ec53600dad7975c454782d
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2022-07-14 21:22:28 -0500

    first commit traversy follow through

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+
+import Button from './Button'
+
+
+const Header = () => {
+
+  return (
+    <header className='header'>
+        <h1>Park's Task Tracker</h1>
+        <Button />
+    </header>
+  )
+}
+
+export default Header
